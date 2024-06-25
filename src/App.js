ff245f73a9c1ce2a6a7f2acdab5188276dcc6279
alien <alien@gmail.com>
2024-06-25 20:16:07 +0800

    feat: init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Othello from './othello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [othello] = useState(new Othello(8, 8));
+    const [board, setBoard] = useState(othello.board);
+    const [blackCount, setBlackCount] = useState(0);
+    const [whiteCount, setWhiteCount] = useState(0);
+
+    const handleCellClick = (row, col) => {
+        if (othello.placePiece(row, col)) {
+            setBoard([...othello.board]);
+            const counts = othello.getPieceCounts();
+            setBlackCount(counts.black);
+            setWhiteCount(counts.white);
+            if (othello.checkGameOver()) {
+                alert('Game Over');
+            }
+        }
+    };
+
+    const handleReset = () => {
+        othello.resetGame();
+        setBoard([...othello.board]);
+        setBlackCount(0);
+        setWhiteCount(0);
+    };
+
+    return (
+        <div>
+            <div className="board">
+                {board.map((row, rowIndex) => (
+                    <div key={rowIndex} className="row">
+                        {row.map((cell, colIndex) => (
+                            <div
+                                key={colIndex}
+                                className={`cell ${cell}`}
+                                onClick={() => handleCellClick(rowIndex, colIndex)}
+                            >
+                                {cell}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className="info">
+                <p>Black: {blackCount}</p>
+                <p>White: {whiteCount}</p>
+            </div>
+            <button onClick={handleReset}>Reset</button>
+        </div>
+    );
+};
 
 export default App;
