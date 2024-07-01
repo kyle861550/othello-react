b5f337f808e0865e04ba16990e59a350d25e7d2a
kyle <kyle861550@gmail.com>
2024-07-01 21:59:14 +0800

    Deploy to GitHub Pages

---
+++ react/precache-manifest.144df5506237022da97137a9730f06e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8da1033d0ae43d5cfc86fe01537c6f4",
+    "url": "/othello-react/react/index.html"
+  },
+  {
+    "revision": "58c88df7d377c24b79a9",
+    "url": "/othello-react/react/static/css/main.e51fab44.chunk.css"
+  },
+  {
+    "revision": "b8cbf85a60940e55f7f6",
+    "url": "/othello-react/react/static/js/2.2357081b.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/othello-react/react/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "58c88df7d377c24b79a9",
+    "url": "/othello-react/react/static/js/main.90c8a7be.chunk.js"
+  },
+  {
+    "revision": "4bef64c458e6c3de0e5b",
+    "url": "/othello-react/react/static/js/runtime~main.6aa15af1.js"
+  }
+]);
